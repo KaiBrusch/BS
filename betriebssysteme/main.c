7eafe4ecfe49eff55af1bdab8c83b14f945b1d0f
kaibrusch <kaibrusch@gmail.com>
2013-11-09 13:33:07 +0100

    Initial Commit

---
+++ betriebssysteme/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  betriebssysteme
+//
+//  Created by kbrusch on 11/9/13.
+//  Copyright (c) 2013 kbrusch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
