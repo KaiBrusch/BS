f7366584988fc62554c2da65c615117fda690cb8
sacry- <nitsche25@gmail.com>
2013-11-09 13:33:39 +0100

    adasd

--- A2/dining.c
+++ A2/dining.c
@@ -3,45 +3,26 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdarg.h>
- 
-#define N 5
-const char *names[N] = { "Aristotle", "Kant", "Spinoza", "Marx", "Russell" };
-pthread_mutex_t forks[N];
- 
-#define lock pthread_mutex_lock
-#define unlock pthread_mutex_unlock
 
-void print(int y, int x, const char *fmt, ...)
+void init_threads()
 {
-	
+	print("initThreads");
 }
- 
-void eat(int id)
-{
+
+void mainLoop(){
+
+	int status = 1;
+	while(status){
+		char[] x = fgets();
+		print(x);
+	}
 
 }
- 
-void think(int id)
-{
 
-}
- 
-void philosophize()
-{
-	print("yolo!");
-}
- 
 int main()
 {
-	int i, id[N];
-	pthread_t tid[N];
- 
-	for (i = 0; i < N; i++)
-		pthread_mutex_init(forks + (id[i] = i), 0);
- 
-	for (i = 0; i < N; i++)
-		pthread_create(tid + i, 0, philosophize, id + i);
- 
-	/* wait forever: the threads don't actually stop */
-	return pthread_join(tid[0], 0);
+	init_threads();
+
+	mainLoop();
+
 }
