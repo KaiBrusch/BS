97941d5c246c0b8d8aefe4138e82065cb37869ee
kaibrusch <kaibrusch@gmail.com>
2013-11-16 12:24:19 +0100

    doxy testing

--- bs/main.h
+++ bs/main.h
@@ -1,3 +1,12 @@
+/** @file main.h
+ *  @brief This file contains all constants and method signatures
+ *
+ * @author Kai Brusch
+ * @author Matthias Nitsche
+ * @author Swaneet Kumar
+ * @author Ivan Morozov
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
@@ -46,16 +55,94 @@
 #define GIVEN_NAMES 5
 
 // methods of main
+
+/** @brief Initialize all philosophers states and create the pthreads.
+ * @return void
+ */
 void init();
+
+/** @brief a continuous loop that listens on user input.
+ *  @return void
+ */
 void inputLoop();
+
+/** @brief a for loop that runs EAT_LOOP times to simulate a waiting time.
+    The Function is also used to block the philosophers for this type of action.
+ *  @param p_id philosopher id
+ *  @return void
+ */
+
 void eat(int p_id);
+
+/** @brief a for loop that runs THINK_LOOP times to simulate a waiting time.
+    The Function is also used to block the philosophers for this type of action.
+ *  @param p_id philosopher id
+ *  @return void
+ */
 void think(int p_id);
+
+/** @brief is the philosopher thread where a philosopher calls "think",
+ *  "get_sticks", "eat" and "put_sticks". A philosopher thread runs until
+ *  it is destroyed by the user through an input in the inputloop starting
+ *  with "q|Q".
+ *  @param *arg a pointer to a arbitrary object (in this case philo id).
+ *  @return void
+ */
 void *philo(void *arg);
+
+/** @brief prints out the current state of each philosopher in a row based
+ *  view within the shell/IDE. The view works like this:
+ *  p_id_0 + p_state_0 + " " .. p_id_NPHILO + p_state_NPHILO + " "
+ *  @return void
+ */
 void disp_philo_states();
+
+/** @brief gets the left and right sticks next to this particular philosopher (p_id).
+ *  To do so the pthread_mutex is locked at the beginning and then unlocked at the end.
+ *  This is to make sure that the sticks won't be used by other philosophers. The state
+ *  of the Philosopher is then changed. The Philosopher then waits for his left and right stick
+ *  to be freed so the philosopher can be set to the State EAT.
+ *  @param p_id philosopher id
+ *  @return void
+ */
 void get_sticks(int p_id);
+
+/** @brief put_sticks is called after get_sticks to set the left and right stick of
+ *  the current Philosoph to UNUSED. This is to ensure that the sticks can be used
+ *  by other philosophers. The Philosopher is set to the state THINK again.
+ *  @param p_id philosopher id
+ *  @return void
+ */
 void put_sticks(int p_id);
+
+/** @brief block_philo takes the p_id of the Philosopher and asks if its state is BLOCKED. If so
+ *  set the state to default and call sem_wait to block him else do nothing.
+ *  @param p_id philosopher id
+ *  @return void
+ */
 void block_philo(int p_id);
+
+/** @brief handle_quit take the first character of the input and checks if it
+ *  is a q or Q. If so release all Philosophers from their semaphore blocks which
+ *  were set through sem_wait. After that join all philosopher so that each philo
+ *  waits for the others. If they were joined the semaphores are destroyed and the
+ *  pthread_mutex are destroyed. The philosophers within their function get a callback
+ *  thorugh the input_command by the user and calls pthread_exit to kill this thread.
+ *  @param first_char is the first input character of the input by the user.
+ *  @return void
+ */
 void handle_quit(char first_char);
+
+/** @brief the handle_command handles the userinput. the cmd_char is at input[1]
+ *  and is a char that must be equal to 'b' for BLOCK, 'u' for UNBLOCK, 'p' for
+ *  PROCEED, else a default is called. The second argument is the philosopher id
+ *  that is taken from input[0] and has to be between 0 and NPHILO. At maximum 9
+ *  Philosophers are allowed. In any case the input_commands array is set to a
+ *  specific state for the Philosopher (p_id).
+ *  @param cmd_char second character of input by user.
+ *  @param p_id philosopher id
+ *  @return void
+ */
 void handle_command(char cmd_char, int p_id);
 
 // TODO:
