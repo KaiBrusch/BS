8cbe1365e5c07a58abd1d3c48fd9df3f993286f6
kaibrusch <kaibrusch@gmail.com>
2013-12-05 16:26:51 +0100

    vmaccess.c comments changed and error messages changed

--- working/vmaccess.c
+++ working/vmaccess.c
@@ -4,49 +4,52 @@
 
 struct vmem_struct *vmem = NULL;
 
-/* Virtual Memory */
+/* Virtual Memory Section */
+
+
 void vm_init(){
-    // connect to shared memory
-    int shm_file_descriptor = shm_open(SHMKEY, O_RDWR, S_IRUSR | S_IWUSR); 
+    // opens connection to shared memory
+    int shm_file_descriptor = shm_open(SHMKEY, O_RDWR, S_IRUSR | S_IWUSR);
     if(!shm_file_descriptor) {
-        perror("shm_open failed!\n");
+        perror("ATTENTION: Failed to open shm_open !\n");
         exit(EXIT_FAILURE);
     }
 
 #ifndef DEBUG_MESSAGES
     else {
-        fprintf(stderr, "shm_open succeeded.\n");
+        fprintf(stderr, "SUCCESS: shm_open successfuly openend.\n");
     }
 #endif
 
-    // set size of shared memory
+    // determining and set size of shared memory
     if(ftruncate(shm_file_descriptor, sizeof(struct vmem_struct)) == -1) {
-        perror("ftruncate failed! Make sure ./mmanage is running!\n");
+        perror("ATTENTION: ftruncate failed! Urgs.. sorry! \n");
         exit(EXIT_FAILURE);
     }
 
 #ifndef DEBUG_MESSAGES
     else {
-        fprintf(stderr, "ftruncate succeeded.\n");
+        fprintf(stderr, "SUCCESS: ftruncate succeeded.\n");
     }
 #endif
 
-    // make shared memory with the variable vmem accessible
-    vmem = mmap(NULL, 
-                sizeof(struct vmem_struct), 
-                PROT_READ | PROT_WRITE, 
-                MAP_SHARED, 
-                shm_file_descriptor, 
+
+    // assign vmem mapping the vmem, this elimantes reference friction
+    vmem = mmap(NULL,
+                sizeof(struct vmem_struct),
+                PROT_READ | PROT_WRITE,
+                MAP_SHARED,
+                shm_file_descriptor,
                 0);
 
     if(!vmem) {
-        perror("mapping into vmem failed!\n");
+        perror("ATTENTION: Failed to map vmem!\n");
         exit(EXIT_FAILURE);
     }
 
 #ifndef DEBUG_MESSAGES
     else {
-        fprintf(stderr, "mapping into vmem succeeded!\n");
+        fprintf(stderr, "SUCCESS: Successfuly mapped into vmem !\n");
     }
 #endif
 
@@ -56,26 +59,26 @@ int vmem_read(int address) {
     if(vmem == NULL) {
         vm_init();
     }
-    
+
     int result;
-    
+
     int page = address / VMEM_PAGESIZE;
     int offset = address % VMEM_PAGESIZE;
 
-    // mark request for the case of a page fault
     vmem->adm.req_pageno = page;
-    
+
     int flags = vmem->pt.entries[page].flags;
-    // check whether the page is currently loaded
+
+    // we need to check if required resource is already present
     int req_page_is_loaded = ((flags & PTF_PRESENT) == PTF_PRESENT);
-    
+
     if (!req_page_is_loaded) {
 	   kill(vmem->adm.mmanage_pid, SIGUSR1);
 	   sem_wait(&vmem->adm.sema);
     }
-    
+
     result = read_page(page, offset);
-    
+
     return result;
 }
 
@@ -83,31 +86,35 @@ void vmem_write(int address, int data) {
     if(vmem == NULL) {
         vm_init();
     }
-    
+
     int page = address / VMEM_PAGESIZE;
     int offset = address % VMEM_PAGESIZE;
-    
-    // mark request for the case of a page fault
+
+
     vmem->adm.req_pageno = page;
-    
+
     int flags = vmem->pt.entries[page].flags;
 
-    // check whether the page is currently loaded
+    // we need to check if required resource is already present
     int is_page_loaded = ((flags & PTF_PRESENT) == PTF_PRESENT);
-    
+
     if (!is_page_loaded) {
        kill(vmem->adm.mmanage_pid, SIGUSR1);
        sem_wait(&vmem->adm.sema);
     }
-    
+
     write_page(page, offset, data);
 }
-/* End Virtual Memory */
+
+
+/* End Virtual Memory Section */
 
 
 
-/* Page Functions */
-int read_page(int page, int offset) {	
+/* Page Functions Section */
+
+
+int read_page(int page, int offset) {
     set_used_bits(page);
     int index = index_from_page_offset(page, offset);
     return vmem->data[index];
@@ -115,19 +122,22 @@ int read_page(int page, int offset) {
 
 void write_page(int page, int offset, int data) {
     set_used_bits(page);
-    
-    // mark the change and to make sure it'll be updated
-    // into the pagefile.bin
+
+    // mark changes in pagefileb.bin
     vmem->pt.entries[page].flags |= PTF_CHANGED;
-    
+
     int index = index_from_page_offset(page, offset);
     vmem->data[index] = data;
 }
-/* End Page Functions */
+
+
+/* End Page Functions Sections */
 
 
 
-/* Helper Functions */
+/* Helper Functions Section */
+
+
 int index_from_page_offset(int page, int offset) {
     return (vmem->pt.entries[page].frame*VMEM_PAGESIZE) + offset;
 }
@@ -141,3 +151,6 @@ void set_used_bits(int page) {
 
     vmem->pt.entries[page].flags |= PTF_USEDBIT1;
 }
+
+
+/* End Helper Functions Section */
