719b20827555bc446513dacd807e208a51ada6d3
sacry- <nitsche25@gmail.com>
2013-12-07 11:56:10 +0100

    small renamings + refactorings

--- working/vmaccess.c
+++ working/vmaccess.c
@@ -4,9 +4,7 @@
 
 struct vmem_struct *vmem = NULL;
 
-/* Virtual Memory Section */
-
-
+/* Virtual Memory */
 void vm_init(){
     // opens connection to shared memory
     int shm_file_descriptor = shm_open(SHMKEY, O_RDWR, S_IRUSR | S_IWUSR);
@@ -35,12 +33,9 @@ void vm_init(){
 
 
     // assign vmem mapping the vmem, this elimantes reference friction
-    vmem = mmap(NULL,
-                sizeof(struct vmem_struct),
+    vmem = mmap(NULL, sizeof(struct vmem_struct),
                 PROT_READ | PROT_WRITE,
-                MAP_SHARED,
-                shm_file_descriptor,
-                0);
+                MAP_SHARED, shm_file_descriptor, 0);
 
     if(!vmem) {
         perror("ATTENTION: Failed to map vmem!\n");
@@ -105,15 +100,11 @@ void vmem_write(int address, int data) {
 
     write_page(page, offset, data);
 }
-
-
-/* End Virtual Memory Section */
+/* End Virtual Memory */
 
 
 
-/* Page Functions Section */
-
-
+/* Page Functions */
 int read_page(int page, int offset) {
     set_used_bits(page);
     int index = index_from_page_offset(page, offset);
@@ -129,15 +120,11 @@ void write_page(int page, int offset, int data) {
     int index = index_from_page_offset(page, offset);
     vmem->data[index] = data;
 }
-
-
-/* End Page Functions Sections */
+/* End Page Functions */
 
 
 
-/* Helper Functions Section */
-
-
+/* Helper Functions */
 int index_from_page_offset(int page, int offset) {
     return (vmem->pt.entries[page].frame*VMEM_PAGESIZE) + offset;
 }
@@ -151,6 +138,3 @@ void set_used_bits(int page) {
 
     vmem->pt.entries[page].flags |= PTF_USEDBIT1;
 }
-
-
-/* End Helper Functions Section */
